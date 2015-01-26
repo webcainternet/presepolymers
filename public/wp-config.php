0d2bae5ab73320b96eaf1d9cb200fc1e38222eaf
fernandofmendes <fernando.mendes@webca.com.br>
2015-01-26 14:17:04 -0200

    config db

--- public/wp-config.php
+++ public/wp-config.php
@@ -14,24 +14,51 @@
  * @package WordPress
  */
 
+
 // ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'presepolymers');
+switch ($_SERVER['SERVER_ADDR'])
+{
+    //Ambiente Local
+    case '127.0.0.1':
+		/** The name of the database for WordPress */
+		define('DB_NAME', 'presepolymers');
 
-/** MySQL database username */
-define('DB_USER', 'root');
+		/** MySQL database username */
+		define('DB_USER', 'root');
 
-/** MySQL database password */
-define('DB_PASSWORD', 'root');
+		/** MySQL database password */
+		define('DB_PASSWORD', 'root');
 
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
+		/** MySQL hostname */
+		define('DB_HOST', 'localhost');
 
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+		/** Database Charset to use in creating database tables. */
+		define('DB_CHARSET', 'utf8');
 
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+		/** The Database Collate type. Don't change this if in doubt. */
+		define('DB_COLLATE', '');
+		break;
+
+	default:
+		/** The name of the database for WordPress */
+		define('DB_NAME', 'presepolymers');
+
+		/** MySQL database username */
+		define('DB_USER', 'presepolymers');
+
+		/** MySQL database password */
+		define('DB_PASSWORD', 'jwUs77yd2y');
+
+		/** MySQL hostname */
+		define('DB_HOST', 'localhost');
+
+		/** Database Charset to use in creating database tables. */
+		define('DB_CHARSET', 'utf8');
+
+		/** The Database Collate type. Don't change this if in doubt. */
+		define('DB_COLLATE', '');
+		break;
+}
 
 /**#@+
  * Authentication Unique Keys and Salts.
